3bb681a41a7afec0ff838b9554542ec29908d607
TrafalgarRicardoLu <trafalgarricardolu@gmail.com>
2018-03-23 22:17:16 +0800

    Add entity file

---
+++ src/main/java/entity/person.java
@@ -0,0 +1,47 @@
+package entity;
+
+public class person {
+
+    private Integer pid;
+
+    private String name;
+
+    private String sex;
+
+    private String age;
+
+    public person() {
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
