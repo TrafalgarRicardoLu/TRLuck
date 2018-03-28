90af5690cb998914941fc4eddb1ef24434916234
TrafalgarRicardoLu <trafalgarricardolu@gmail.com>
2018-03-28 19:59:56 +0800

    Update the file structure and modify index.jsp

--- src/main/java/controller/PersonController.java
+++ src/main/java/controller/PersonController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-
 public class PersonController {
 
-    @RequestMapping(value = "index",method = RequestMethod.GET)
+    @RequestMapping(value = "/",method = RequestMethod.GET)
     public String index(){
         return "index";
     }
