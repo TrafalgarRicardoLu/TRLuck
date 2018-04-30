2ac20f24051b0149dbcbb60d1f4680f5ec6ef237
TrafalgarRicardoLu <trafalgarricardolu@gmail.com>
2018-04-30 15:40:08 +0800

    feat: use select to change authority

--- com.us.ac/src/main/webapp/assets/main/js/main.js
+++ com.us.ac/src/main/webapp/assets/main/js/main.js
@@ -40,15 +40,13 @@ $(function () {
                     }
                 }
             }, {
-                field: "authority.role",
-                title: "Role",
+                field: 'authority.role',
+                title: 'Role',
                 align: 'center',
                 editable: {
-                    type: 'text',
+                    type: 'select',
                     title: 'Role',
-                    validate: function (v) {
-                        if (!v) return 'Can not be null';
-                    }
+                    source:[{value:"ROLE_ADMIN",text:"Admin"},{value:"ROLE_USER",text:"User"}]
                 }
             }, {
                 title: "Operations",
@@ -56,8 +54,13 @@ $(function () {
                 formatter: operateFormatter
             }],
             onEditableSave: function (field, row, oldValue, $el) {
-                var aid = [row.aid];
-                var url = "http://localhost:8080/admin/" + aid;
+                var data= JSON.stringify(row);
+                var begin = data.indexOf(field);
+                var role = data.substr(begin+field.length+3);
+                var end = role.indexOf('"');
+                role=role.substr(0,end);
+                row.authority.role=role;
+                var url = "http://localhost:8080/admin/" + row.aid;
                 $.ajax({
                     type: "PUT",
                     url: url,
@@ -80,6 +83,7 @@ $(function () {
             }
 
         });
+
 
         function operateFormatter(value, row, index) {
             var aid = [row.aid];
